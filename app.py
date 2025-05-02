778d9e666be04988838f0b801a9a9ff222aaa69d
Adam <272292@student.pwr.edu.pl>
2025-05-02 20:31:12 +0200

    add descriptions

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ st.caption("Upload a PNG or JPG image. JPG allows only encoding. Output will alw
 
 #upload box
 uploaded_file = st.file_uploader("Upload Image", type=["png", "jpg", "jpeg"])
+uploaded_file.name = uploaded_file.name.lower()
 
 #process uploaded file
 if uploaded_file:
