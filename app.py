318699a66bb14f64aaecf8e990647c66c33405fe
Adam <272292@student.pwr.edu.pl>
2025-05-02 21:05:52 +0200

    safer

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+import html
 import os
 import io
 import dekodowanie
@@ -24,6 +25,7 @@ st.caption("Upload a PNG or JPG image. JPG allows only encoding. Output will alw
 #upload box
 uploaded_file = st.file_uploader("Upload Image", accept_multiple_files=False)
 
+#makes extentions lowercase befor streamlit check extention so it doesnt panic
 if uploaded_file:
     ext = os.path.splitext(uploaded_file.name)[1].lower()
     if ext not in [".png", ".jpg", ".jpeg"]:
@@ -48,31 +50,35 @@ if uploaded_file:
             st.caption("🔒 Decoding is only supported for PNG files.")
         else:
             if st.button("🔓 Decode"):
-                decoded = dekodowanie.dekodowanie(img.copy())
+                decoded = html.escape(dekodowanie.dekodowanie(img.copy()))
                 st.subheader("Decoded Text:")
                 st.code(decoded.replace("\\n", "\n"))
 
 #coding section
     with col2:
         #input box
-        text_to_encode = st.text_area("Enter text to encode:")
+        text_to_encode = html.escape(st.text_area("Enter text to encode:"))
 
         if st.button("🔐 Encode"):
-            if text_to_encode:
-                #processing img
-                encoded_img = kodowanie.kodowanie(text_to_encode, img.copy())
-                st.subheader("Modified Image (PNG format):")
-                st.image(encoded_img, caption="Image with Encoded Text", use_container_width=True)
-                buf = io.BytesIO()
-                encoded_img.save(buf, format="PNG")
-
-                #filename handling
-                filename = uploaded_file.name
-                base_name = os.path.splitext(filename)[0]
-                mod_filename = f"{base_name}_mod.png"
-
-                #download button
-                st.download_button("💾 Download Encoded Image", data=buf.getvalue(), file_name=mod_filename, mime="image/png")
-
+            max_chars = (img.width // 16) * (img.height // 16)
+            if len(text_to_encode) > max_chars:
+                st.error("Message is too long for this image.")
             else:
-                st.warning("Please enter some text to encode.")
+                if text_to_encode:
+                    #processing img
+                    encoded_img = kodowanie.kodowanie(text_to_encode, img.copy())
+                    st.subheader("Modified Image (PNG format):")
+                    st.image(encoded_img, caption="Image with Encoded Text", use_container_width=True)
+                    buf = io.BytesIO()
+                    encoded_img.save(buf, format="PNG")
+
+                    #filename handling
+                    filename = uploaded_file.name
+                    base_name = os.path.splitext(filename)[0]
+                    mod_filename = f"{base_name}_mod.png"
+
+                    #download button
+                    st.download_button("💾 Download Encoded Image", data=buf.getvalue(), file_name=mod_filename, mime="image/png")
+
+                else:
+                    st.warning("Please enter some text to encode.")
